b8cc7da93260321aeadfa334d71e5e6a20e2d879
Etienne Schmidt <gladion136@googlemail.com>
2024-10-12 14:22:00 +0200

    fix: missing subcommand
    Signed-off-by: Etienne Schmidt <gladion136@googlemail.com>

--- cargo-diagram/src/main.rs
+++ cargo-diagram/src/main.rs
@@ -6,14 +6,28 @@ use analyzer::analyze_repository;
 use cargo_diagram_printers::uml::print_uml_to_file;
 use cargo_diagram_printers::PrintOptions;
 use cargo_diagram_visitors::module_visitor::ModulesVisitor;
+use clap::Subcommand;
 use clap::{command, Parser};
 
 mod analyzer;
 
+#[derive(Parser)]
+#[command(name = "cargo-diagram", about = "A tool for generating diagrams")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Creates diagrams about your crate
+    Diagram(DiagramArgs),
+}
+
 /// Creates diagrams about your crate
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
+struct DiagramArgs {
     /// Show relations inside of the diagram (alpha)
     #[arg(short, long, default_value_t = false)]
     relations: bool,
@@ -40,23 +54,28 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
+    let cli = Cli::parse();
 
-    let project_root = &args.path;
+    // Match the subcommand and handle logic
+    match cli.command {
+        Commands::Diagram(args) => {
+            let project_root = &args.path;
 
-    let mut visitor = ModulesVisitor {
-        module_map: BTreeMap::new(),
-        current_module: String::new(),
-    };
+            let mut visitor = ModulesVisitor {
+                module_map: BTreeMap::new(),
+                current_module: String::new(),
+            };
 
-    let options = PrintOptions {
-        relations: args.relations,
-        module_color: args.module_color,
-        trait_color: args.trait_color,
-        functions_private: args.functions_private,
-    };
+            let options = PrintOptions {
+                relations: args.relations,
+                module_color: args.module_color,
+                trait_color: args.trait_color,
+                functions_private: args.functions_private,
+            };
 
-    analyze_repository(project_root, &mut visitor);
+            analyze_repository(project_root, &mut visitor);
 
-    print_uml_to_file(&visitor, &PathBuf::from(args.output), options);
+            print_uml_to_file(&visitor, &PathBuf::from(args.output), options);
+        }
+    }
 }
--- tools/regenerate.sh
+++ tools/regenerate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cargo r --bin cargo-diagram -- -o ./docs/assets/overview.puml
+cargo r --bin cargo-diagram -- diagram -o ./docs/assets/overview.puml
 plantuml ./docs/assets/overview.puml -tsvg
 plantuml ./docs/assets/overview.puml
 
-cargo r --bin cargo-diagram -- -r -o ./docs/assets/overview_detailed.puml
+cargo r --bin cargo-diagram -- diagram -r -f -o ./docs/assets/overview_detailed.puml
 plantuml ./docs/assets/overview_detailed.puml -tsvg
 plantuml ./docs/assets/overview_detailed.puml
